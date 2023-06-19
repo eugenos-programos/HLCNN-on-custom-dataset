e58f86839f6cd330f6a9d3866eccc6ce00ed61b4
eugenos-programos <elena.samchuk.73@mail.ru>
2023-06-19 23:25:00 +0300

    feat(hlcnn): add custom preprocess methods to data class

--- custom_dataset.py
+++ custom_dataset.py
@@ -2,13 +2,45 @@ import torch
 import os
 import torchvision
 from PIL import Image
+import numpy as np
+from skimage import transform as sktransform
 
 
-class BoxesDataset(torch.utils.data.Dataset):
+
+class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, root):
         self.root = root
         self.imgs = os.listdir(os.path.join(self.root, "images"))
         self.labels = os.listdir(os.path.join(self.root, "labels"))
+
+    def preprocess(self, img, min_size=720, max_size=1280):
+        H, W, C = img.shape
+        scale1 = min_size / min(H, W)
+        scale2 = max_size / max(H, W)
+        scale = min(scale1, scale2)
+        img = img / 255.
+        img = sktransform.resize(
+            img, (int(H * scale), int(W * scale), C), mode='reflect', anti_aliasing=True)
+        img = np.asarray(img, dtype=np.float32)
+        return img
+
+    def resize_bbox(self, bbox, in_size, out_size):
+        bbox = bbox.copy()
+        x_scale = float(out_size[0]) / in_size[0]
+        y_scale = float(out_size[1]) / in_size[1]
+        bbox[:, 0] = np.round(y_scale * bbox[:, 0])
+        bbox[:, 2] = np.round(y_scale * bbox[:, 2])
+        bbox[:, 1] = np.round(x_scale * bbox[:, 1])
+        bbox[:, 3] = np.round(x_scale * bbox[:, 3])
+        return bbox
+
+    def read_gt_bbox(self, annoFile):
+        gt_boxes = []
+        for line in annoFile:
+            bbox = line.split()
+            gt_boxes.append([int(bbox[0])+1, int(bbox[1])+1, int(bbox[2]) -
+                            int(bbox[0])+1, int(bbox[3])-int(bbox[1]), int(bbox[4])])
+        return gt_boxes
 
     def __transform_boxes_coords__(self, boxes_coords, img_size=640):
         center_x, center_y, width, height = boxes_coords
